d6d25408e6174471e5fbd60e6490df415ee78a1c
Sunnex 3 <144198043+SunnexGB@users.noreply.github.com>
2025-05-25 12:35:37 +0700

    ip host update

--- FixSpotify-Linux.sh
+++ FixSpotify-Linux.sh
@@ -4,28 +4,17 @@ echo "Updating hosts file: $HOSTS_FILE"
 echo "Run as a user with sudo(if required)."
 
 SPOTIFY_ENTRIES=$(cat <<EOF
-50.7.85.220 spotify.com
-50.7.85.220 api.spotify.com
-50.7.85.220 appresolve.spotify.com
-50.7.85.220 accounts.spotify.com
-50.7.85.220 aet.spotify.com
-50.7.85.220 gew1-spclient.spotify.com
-50.7.85.220 login5.spotify.com
-50.7.85.220 open.spotify.com
-50.7.85.220 spotifycdn.com
-50.7.85.220 www.spotify.com
-50.7.85.220 spotifycdn.com
-
-52.24.63.106 api.spotify.com
-54.230.200.102 appresolve.spotify.com
-52.84.211.27 accounts.spotify.com
-3.220.229.198 aet.spotify.com
-52.88.125.19 gew1-spclient.spotify.com
-34.202.248.102 login5.spotify.com
-34.236.183.125 open.spotify.com
-54.174.128.23 spotifycdn.com
-54.174.182.159 www.spotify.com
-54.174.128.23 spotifycdn.com
+204.12.192.220 gew1-spclient.spotify.com
+204.12.192.220 login5.spotify.com
+204.12.192.220 spotify.com
+204.12.192.220 api.spotify.com
+204.12.192.220 appresolve.spotify.com
+204.12.192.220 accounts.spotify.com
+204.12.192.220 aet.spotify.com
+204.12.192.220 open.spotify.com
+204.12.192.220 spotifycdn.com
+204.12.192.220 www.spotify.com
+204.12.192.220 spotifycdn.com
 EOF
 )
 
--- FixSpotify-Windows.bat
+++ FixSpotify-Windows.bat
@@ -8,31 +8,17 @@ echo Requires running as administrator.
 
 REM
 (
-    echo 50.7.85.220 spotify.com
-    echo 50.7.85.220 api.spotify.com
-    echo 50.7.85.220 appresolve.spotify.com
-    echo 50.7.85.220 accounts.spotify.com
-    echo 50.7.85.220 aet.spotify.com
-    echo 50.7.85.220 gew1-spclient.spotify.com
-    echo 50.7.85.220 login5.spotify.com
-    echo 50.7.85.220 open.spotify.com
-    echo 50.7.85.220 spotifycdn.com
-    echo 50.7.85.220 www.spotify.com
-    echo 50.7.85.220 spotifycdn.com
-) >> "%hostsFile%"
-
-REM
-(
-    echo 52.24.63.106 api.spotify.com
-    echo 54.230.200.102 appresolve.spotify.com
-    echo 52.84.211.27 accounts.spotify.com
-    echo 3.220.229.198 aet.spotify.com
-    echo 52.88.125.19 gew1-spclient.spotify.com
-    echo 34.202.248.102 login5.spotify.com
-    echo 34.236.183.125 open.spotify.com
-    echo 54.174.128.23 spotifycdn.com
-    echo 54.174.182.159 www.spotify.com
-    echo 54.174.128.23 spotifycdn.com
+echo 204.12.192.220 gew1-spclient.spotify.com
+echo 204.12.192.220 login5.spotify.com
+echo 204.12.192.220 spotify.com
+echo 204.12.192.220 api.spotify.com
+echo 204.12.192.220 appresolve.spotify.com
+echo 204.12.192.220 accounts.spotify.com
+echo 204.12.192.220 aet.spotify.com
+echo 204.12.192.220 open.spotify.com
+echo 204.12.192.220 spotifycdn.com
+echo 204.12.192.220 www.spotify.com
+echo 204.12.192.220 spotifycdn.com
 ) >> "%hostsFile%"
 
 echo File succes update.
